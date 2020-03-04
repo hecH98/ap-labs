e059122cb33a1bf719fa4fa0a22d51451271b4cb
Hector Herrera <herrera.hector1998@gmail.com>
2020-03-04 10:27:57 -0600

    string manipulation completed

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char *);
+
+int main(int argc, char** argv) {
+    if(argc <= 3) {
+        printf("Usage:\n");
+        printf("./nameOfProgram originalString addition add\n");
+        return 0;
+    }
+
+    int strLength = mystrlen(argv[1]);
+    printf("Initial length:     : %d\n", strLength);
+    char* strConcat = mystradd(argv[1], argv[2]);
+    printf("New String          : %s\n",strConcat);
+    int pos = mystrfind(strConcat, argv[3]);
+    printf("SubString was found : ");
+    if(pos != -1) {
+        printf("yes, in position: %d\n", pos);
+    }
+    else {
+        printf("no\n");
+    }
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,41 @@
-
-
+#include <stdlib.h>
 int mystrlen(char *str){
-    return 0;
+    int i = 0;
+    while (str[i] != '\0'){
+        i++;
+    }
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char* res = malloc(sizeof(char));
+    int i = 0, j = 0;
+    while(origin[i] != '\0') {
+        res[j++] = origin[i++];
+    }
+    i = 0;
+    while(addition[i] != '\0') {
+        res[j++] = addition[i++];
+    }
+    return res;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int sub = 0;
+    int orig = 0;
+    int subLength = mystrlen(substr);
+    while(origin[orig] != '\0'){
+        if(origin[orig] == substr[sub]){
+            sub++;
+            if(sub == subLength) {
+                break;
+            }
+            orig++;
+        }
+        else {
+            orig++;
+            sub=0;
+        }
+    }
+    return sub ? orig-sub+1 : -1;
 }
