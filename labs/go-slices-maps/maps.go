a9924294f8554c345c4b8edfefbce25333cf77df
Hector Herrera <herrera.hector1998@gmail.com>
2020-03-10 18:06:01 -0600

    maps slices completed

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
-func main() {
-}
+ import (
+ 	"golang.org/x/tour/wc"
+ 	"strings"
+ )
+
+ func WordCount(s string) map[string]int {
+ 	array := strings.Fields(s)
+ 	map2 := make(map[string]int)
+ 	for _,v := range array {
+ 		map2[v]++
+ 	}
+ 	return map2
+ }
+
+ func main() {
+ 	wc.Test(WordCount)
+ }
