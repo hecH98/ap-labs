c0497c8bde68714bd296cc749c09772ccfe1f1d3
Hector Herrera <herrera.hector1998@gmail.com>
2020-03-23 17:30:29 -0600

    mycat completed

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define BUFSIZE 50
 
 /* filecopy:  copy file ifp to file ofp */
-void filecopy(FILE *ifp, FILE *ofp)
+void filecopy(int ifp, int ofp)
 {
-    int c;
-
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
+    int c[BUFSIZE];
+    int number;
+    while ( (number = read(ifp, c, BUFSIZE)) > 0) {
+	write(ofp, c, BUFSIZE);
+    }
 
 }
 
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
+    int fp;
+    void filecopy(int, int);
     char *prog = argv[0];   /* program name for errors */
 
     if (argc == 1)  /* no args; copy standard input */
-        filecopy(stdin, stdout);
+        filecopy(1, 0);
     else
         while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
-                fprintf(stderr, "%s: can′t open %s\n",
-			prog, *argv);
+            if ((fp = open(*++argv, O_RDONLY, 0)) == -1) {
+                fprintf(stderr, "%s: can′t open %s\n", prog, *argv);
                 return 1;
             } else {
-                filecopy(fp, stdout);
-                fclose(fp);
+                filecopy(fp, 0);
+                close(fp);
             }
 
     if (ferror(stdout)) {
