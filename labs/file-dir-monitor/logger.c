9c37c3c519271f56628aad3bd88f041cbac4da91
Hector Herrera <herrera.hector1998@gmail.com>
2020-05-12 15:26:47 -0500

    file/directory monitor completed

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,103 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <string.h>
+#include <syslog.h>
 #include "logger.h"
+#define RESET		0
+#define BRIGHT 		1
+#define DIM		    2
+#define UNDERLINE 	3
+#define BLINK		4
+#define REVERSE		7
+#define HIDDEN		8
+#define BLACK 		0
+#define RED		    1
+#define GREEN		2
+#define YELLOW		3
+#define BLUE		4
+#define MAGENTA		5
+#define CYAN		6
+#define	WHITE		7
+
+int boolLog = 0;
 
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+    if(strcmp(logType,"syslog") == 0){
+        boolLog = 1;
+        printf("Initializing Logger on: %s\n", logType);
+    } else {
+        printf("Logger not initialized in syslog\n");
+    }
+    
     return 0;
+}
+
+void colorText(int text, int background) {
+    char command[13];
+	sprintf(command, "%c[0;%d;%dm", 0x1B,text + 30, background + 1);
+	printf("%s", command);
 }
 
 int infof(const char *format, ...) {
-    return 0;
+	colorText(WHITE, BLACK);
+	va_list input;
+	va_start(input, format);
+    if(boolLog) {
+        openlog("advancedLogger",LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_INFO,format,input);
+        closelog();
+    } else {
+        vprintf(format, input);
+    }
+	va_end(input);
+	return 0;
 }
 
 int warnf(const char *format, ...) {
-    return 0;
+	colorText(YELLOW, BLACK);
+    va_list input;
+    va_start(input, format);
+    if(boolLog) {
+        openlog("advancedLogger",LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_WARNING,format,input);
+        closelog();
+    } else {
+        vprintf(format, input);
+    }
+    va_end(input);
+	colorText(WHITE, BLACK);
+	return 0;
 }
 
 int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
+	colorText(RED, BLACK);
+    va_list input;
+    va_start(input, format);
+    if(boolLog) {
+        openlog("advancedLogger",LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_ERR,format,input);
+        closelog();
+    } else {
+        vprintf(format, input);
+    }
+    va_end(input);
+    colorText(WHITE, BLACK);
     return 0;
 }
 
+int panicf(const char *format, ...) {
+    colorText(MAGENTA, BLACK);
+    va_list input;
+    va_start(input, format);
+    if(boolLog) {
+        openlog("advancedLogger",LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_CRIT,format,input);
+        closelog();
+    } else {
+        vprintf(format, input);
+    }
+    va_end(input);
+	raise(SIGABRT);
+    return -1;
+}
--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
+#include <ftw.h>
+#include <unistd.h>
+#include <sys/inotify.h>
 #include "logger.h"
 
-int main(){
-    // Place your magic here
+#define EVENT_LENGTH (1024 * ((sizeof(struct inotify_event)) + 16))
+
+struct inotify_event *event;
+
+int main(int argc, char** argv){
+	int initNotify;
+	int watchDir;
+	int info;
+	char buff[EVENT_LENGTH];
+	if(argc < 2) {
+        warnf("USAGE: ./monitor \"path\"\n");
+        return -1;
+    }
+    infof("Reading path %s\n", argv[1]);
+    if((initNotify = inotify_init()) == -1) {
+        warnf("Error initializing inotify\n");
+    }
+    if((watchDir = inotify_add_watch(initNotify, argv[1], IN_ALL_EVENTS)) == -1){
+        warnf("Error watching directory\n");
+    }
+    while(1) {
+        info = read(initNotify, buff, EVENT_LENGTH);
+        for (int i = 0; i < info; 1) {
+            event = (struct inotify_event * ) &buff[i];
+            if (event->mask & IN_ACCESS) {
+                infof("File was accessed\n");
+            } else if (event->mask & IN_ATTRIB) {
+                infof("Metadata changed—for example, permissions\n");
+            } else if (event->mask & IN_CLOSE_WRITE) {
+                infof("File opened for writing was closed\n");
+            } else if (event->mask & IN_CLOSE_NOWRITE) {
+                infof("File or directory not opened for writing was closed\n");
+            } else if (event->mask & IN_CREATE) {
+                infof("File/directory created in watched directory\n");
+            } else if (event->mask & IN_DELETE) {
+                infof("ile/directory deleted from watched directory\n");
+            } else if (event->mask & IN_DELETE_SELF) {
+                infof("Watched file/directory was itself deleted\n");
+            } else if (event->mask & IN_MODIFY) {
+                infof("File was modified\n");
+            } else if (event->mask & IN_MOVE_SELF) {
+                infof("Watched file/directory was itself moved\n");
+            } else if (event->mask & IN_MOVED_FROM) {
+                infof("Generated for the directory containing the old filename when a file is renamed\n");
+            } else if (event->mask & IN_MOVED_TO) {
+                infof("Generated for the directory containing the new filename when a file is renamed\n");
+            } else if (event->mask & IN_OPEN) {
+                infof("File or directory was opened\n");
+            }
+            i += ( sizeof (struct inotify_event) ) + event->len;
+        }
+    }
+    inotify_rm_watch(initNotify, watchDir);
+    close(initNotify);
     return 0;
 }
